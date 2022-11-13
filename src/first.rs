419a46b4bc3e880e325a091dc179316355d42040
EncodePanda <paul.szulc@gmail.com>
2022-11-13 23:46:04 +0100

    Implement simple semantics where List is just enum

--- src/first.rs
+++ src/first.rs
@@ -1,30 +1,66 @@
 use std::mem;
 
 pub struct List {
-    head: Link
+  head: Node,
 }
 
-pub struct Node {
-    elem: i32,
-    next: Link
-}
-
-pub enum Link {
-    Empty,
-    More(Box<Node>)
+pub enum Node {
+  Empty,
+  Next(i32, Box<Node>),
 }
 
 impl List {
+  pub fn new() -> Self {
+    List { head: Node::Empty }
+  }
 
-    pub fn new() -> Self {
-	List { head: Link::Empty}
+  pub fn push(&mut self, elem: i32) {
+    match mem::replace(&mut self.head, Node::Empty) {
+      Node::Empty => self.head = Node::Next(elem, Box::new(Node::Empty)),
+      tail => {
+        let node = Node::Next(elem, Box::new(tail));
+        self.head = node
+      }
     }
-    pub fn push(&mut self, elem: i32) {
-	let newNode = Node {
-	    elem: elem,
-	    next: mem::replace(&mut self.head, Link::Empty)
-	};
-	self.head = Link::More(Box::new(newNode))
+  }
 
+  pub fn pop(&mut self) -> Option<i32> {
+    match mem::replace(&mut self.head, Node::Empty) {
+      Node::Empty => None,
+      Node::Next(elem, node) => {
+        self.head = *node;
+        Some(elem)
+      }
     }
+  }
+}
+
+mod test {
+  #[test]
+  fn basics() {
+    let mut list = super::List::new();
+
+    // Check empty list behaves right
+    assert_eq!(list.pop(), None);
+    // Populate list
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    // Check normal removal
+    assert_eq!(list.pop(), Some(3));
+    assert_eq!(list.pop(), Some(2));
+
+    // Push some more just to make sure nothing's corrupted
+    list.push(4);
+    list.push(5);
+
+    // Check normal removal
+    assert_eq!(list.pop(), Some(5));
+    assert_eq!(list.pop(), Some(4));
+
+    // Check exhaustion
+    assert_eq!(list.pop(), Some(1));
+    assert_eq!(list.pop(), None);
+  }
 }
