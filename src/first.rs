ff3f08e5e7786095ada806f841ecfbe074a1fb27
EncodePanda <paul.szulc@gmail.com>
2022-11-13 23:47:11 +0100

    Add rustfmt config with 2 indentation

--- src/first.rs
+++ src/first.rs
@@ -1,30 +1,28 @@
 use std::mem;
 
 pub struct List {
-    head: Link
+  head: Link,
 }
 
 pub struct Node {
-    elem: i32,
-    next: Link
+  elem: i32,
+  next: Link,
 }
 
 pub enum Link {
-    Empty,
-    More(Box<Node>)
+  Empty,
+  More(Box<Node>),
 }
 
 impl List {
-
-    pub fn new() -> Self {
-	List { head: Link::Empty}
-    }
-    pub fn push(&mut self, elem: i32) {
-	let newNode = Node {
-	    elem: elem,
-	    next: mem::replace(&mut self.head, Link::Empty)
-	};
-	self.head = Link::More(Box::new(newNode))
-
-    }
+  pub fn new() -> Self {
+    List { head: Link::Empty }
+  }
+  pub fn push(&mut self, elem: i32) {
+    let newNode = Node {
+      elem: elem,
+      next: mem::replace(&mut self.head, Link::Empty),
+    };
+    self.head = Link::More(Box::new(newNode))
+  }
 }
