8f68a4c53e3920dc3dbc81b0c8b0c49ebffcd99b
EncodePanda <paul.szulc@gmail.com>
2022-11-17 12:48:18 +0100

    Add pop and tests to first.rs

--- src/first.rs
+++ src/first.rs
@@ -19,10 +19,49 @@ impl List {
     List { head: Link::Empty }
   }
   pub fn push(&mut self, elem: i32) {
-    let newNode = Node {
+    let new_node = Node {
       elem: elem,
       next: mem::replace(&mut self.head, Link::Empty),
     };
-    self.head = Link::More(Box::new(newNode))
+    self.head = Link::More(Box::new(new_node))
+  }
+  pub fn pop(&mut self) -> Option<i32> {
+    match mem::replace(&mut self.head, Link::Empty) {
+      Link::Empty => None,
+      Link::More(node) => {
+        self.head = node.next;
+        Some(node.elem)
+      }
+    }
+  }
+}
+
+mod test {
+  #[test]
+  fn basics() {
+    let mut list = super::List::new();
+
+    // Check empty list behaves right
+    assert_eq!(list.pop(), None);
+    // Populate list
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    // Check normal removal
+    assert_eq!(list.pop(), Some(3));
+    assert_eq!(list.pop(), Some(2));
+
+    // Push some more just to make sure nothing's corrupted
+    list.push(4);
+    list.push(5);
+
+    // Check normal removal
+    assert_eq!(list.pop(), Some(5));
+    assert_eq!(list.pop(), Some(4));
+
+    // Check exhaustion
+    assert_eq!(list.pop(), Some(1));
+    assert_eq!(list.pop(), None);
   }
 }
